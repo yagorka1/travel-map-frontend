81282df73ca446856070916601891b9cac51dff2
Egor Grinevich <yahor_hrynevich@velmie.com>
2025-10-30 10:28:54 +0100

    feat: notifications

---
+++ libs/core/src/lib/ui/notification/components/notification/notification.component.spec.ts
@@ -0,0 +1,29 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { NotificationComponent } from './notification.component';
+import { NotificationInterface } from '../../interfaces/notification.interface';
+import { NotificationTypeEnum } from '../../enums/notification-type.enum';
+
+describe('NotificationComponent', () => {
+  let component: NotificationComponent;
+  let fixture: ComponentFixture<NotificationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NotificationComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(NotificationComponent);
+    component = fixture.componentInstance;
+
+    component.notification = {
+      type: NotificationTypeEnum.Success,
+      message: 'Test',
+    } as NotificationInterface;
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ libs/core/src/lib/ui/notification/components/notification/notification.component.ts
@@ -0,0 +1,38 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { NotificationStyles } from '../../constants/notification-styles.const';
+import { NotificationInterface } from '../../interfaces/notification.interface';
+
+const NOTIFICATION_DURATION = 5000;
+
+@Component({
+  selector: 'lib-notification',
+  templateUrl: './notification.component.html',
+})
+export class NotificationComponent implements OnInit {
+  @Input() notification!: NotificationInterface;
+
+  @Output() closed = new EventEmitter<void>();
+
+  public data!: { iconName: string; iconClass: string; icon: string };
+
+  public visible = true;
+
+  private readonly config = NotificationStyles;
+
+  public ngOnInit() {
+    this.data = {
+      icon: this.config[this.notification.type].icon,
+      iconClass: this.config[this.notification.type].class,
+      iconName: this.config[this.notification.type].iconName,
+    };
+
+    if (!this.notification.notHiding) {
+      setTimeout(() => this.close(), NOTIFICATION_DURATION);
+    }
+  }
+
+  public close() {
+    this.visible = false;
+    this.closed.emit();
+  }
+}
