ce24de25c03c772c771363b7fec95ad6b6e2c992
Egor Grinevich <yahor_hrynevich@velmie.com>
2025-12-26 12:44:49 +0100

    feat: update forms

--- apps/travel-map-frontend/src/app/core/components/layouts/aside-menu/aside-menu.component.ts
+++ apps/travel-map-frontend/src/app/core/components/layouts/aside-menu/aside-menu.component.ts
@@ -1,31 +1,30 @@
-import { Component, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, Signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { AuthService, SpinnerService } from '@app/core';
 import { TranslatePipe } from '@ngx-translate/core';
 import { UnreadMessagesService } from '../../../../pages/chats/services/unread-messages.service';
 import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
-import { NgOptimizedImage } from '@angular/common';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @UntilDestroy()
 @Component({
   selector: 'app-aside-menu',
-  imports: [RouterLink, TranslatePipe, NgOptimizedImage],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './aside-menu.component.html',
   styleUrl: './aside-menu.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AsideMenuComponent {
-  public unreadMessages = 0;
-
   private unreadMessagesService = inject(UnreadMessagesService);
   private authService = inject(AuthService);
   private spinnerService = inject(SpinnerService);
 
+  public readonly unreadMessages: Signal<number | null> = toSignal(this.unreadMessagesService.totalUnread$, {
+    initialValue: 0,
+  });
+
   constructor() {
     this.unreadMessagesService.initialize();
-
-    this.unreadMessagesService.totalUnread$.subscribe((totalUnread) => {
-      this.unreadMessages = totalUnread || 0;
-    });
 
     this.unreadMessagesService.setInitialUnreadMessagesCount();
   }
--- apps/travel-map-frontend/src/app/pages/trips/components/trip-detail/trip-detail.component.ts
+++ apps/travel-map-frontend/src/app/pages/trips/components/trip-detail/trip-detail.component.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import { TranslateModule } from '@ngx-translate/core';
 import { TripInterface } from '../../interfaces/trip.interface';
 import { TripsService } from '../../services/trips.service';
 import { MapComponent } from '../map/map.component';
 import { SpinnerService } from '@app/core';
+import { Observable } from 'rxjs';
 
-@UntilDestroy()
 @Component({
   selector: 'app-trip-detail',
   imports: [CommonModule, TranslateModule, MapComponent],
@@ -20,19 +19,10 @@ export class TripDetailComponent implements OnInit {
   private tripService: TripsService = inject(TripsService);
   private route: ActivatedRoute = inject(ActivatedRoute);
   private id: string = this.route.snapshot.params['id'];
-  public trip: TripInterface | null = null;
+  public trip$: Observable<TripInterface> | null = null;
   private spinnerService: SpinnerService = inject(SpinnerService);
 
   public ngOnInit(): void {
-    this.spinnerService
-      .show(this.tripService.getTrip(this.id))
-      .pipe(untilDestroyed(this))
-      .subscribe((trip: TripInterface) => {
-        this.trip = trip;
-      });
-  }
-
-  public get tripArray(): TripInterface[] | null {
-    return this.trip ? [this.trip] : null;
+    this.trip$ = this.spinnerService.show(this.tripService.getTrip(this.id));
   }
 }
